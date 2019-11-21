adf59fbf5188074e84a4f9de1f23b236e55a28cb
Steven Gerard <stevengerard@Stevens-MacBook-Pro.local>
2019-11-20 19:07:28 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
+library.add(faBars);
 
-function App() {
+class App extends Component {
+  constructor (){
+    super();
+    this.state = {
+      toggleMenu: false
+    }
+  }
+
+  handleToggle = () => {
+    this.setState({
+      toggleMenu: !this.state.toggleMenu
+    })
+  }
+  
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {!this.state.toggleMenu     
+    ? (<header id='header'>
+        <a className="boot">Start Bootstrap</a>
+        <nav id='nav'>
+          <button className="nav-links">SERVICES</button>
+          <button className="nav-links">PORTFOLIO</button>
+          <button className="nav-links">ABOUT</button>
+          <button className="nav-links">TEAM</button>
+          <button className="nav-links">CONTACT</button>
+        </nav>
+        <button id='ham-button' onClick={this.handleToggle}>MENU
+          <FontAwesomeIcon icon="bars" id='hamburger'/>
+        </button>
+      </header>)
+      : (
+        <>
+        <header id='header'>
+        <a className="boot">Start Bootstrap</a>
+        <nav id='nav'>
+          <button className="nav-links">SERVICES</button>
+          <button className="nav-links">PORTFOLIO</button>
+          <button className="nav-links">ABOUT</button>
+          <button className="nav-links">TEAM</button>
+          <button className="nav-links">CONTACT</button>
+        </nav>
+        <button id='ham-button' onClick={this.handleToggle}>MENU
+          <FontAwesomeIcon icon="bars" id='hamburger'/>
+        </button>
       </header>
+      <nav id='side-menu'>
+              <a className='drop-link'>SERVICES</a>
+              <a className='drop-link'>PORTFOLIO</a>
+              <a className='drop-link'>ABOUT</a>
+              <a className='drop-link'>TEAM</a>
+              <a className='drop-link'>CONTACT</a>
+            </nav>
+      </>)}
+      <div id='container-div'>
+        <div id='center-text'>
+          <div id='welcome'>Welcome To Our Studio!</div>
+          <div id='nice'><strong>IT'S NICE TO MEET YOU</strong></div>
+          <button id='button'>TELL ME MORE</button>
+        </div>
+      </div>
     </div>
   );
+}
 }
 
 export default App;
